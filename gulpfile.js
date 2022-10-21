072ebbe0382e31bee1f94ec9b3fbb374e80423ec
Andrew Schastlivcev <andrew.schastlivcev@sparkybit.com>
2022-10-21 12:49:20 +0300

    Update packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
-const { src, dest, watch, series } = require('gulp');
-const sass = require('gulp-sass');
-const postcss = require('gulp-postcss');
-const cssnano = require('cssnano');
-const terser = require('gulp-terser');
-const browsersync = require('browser-sync').create();
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const terser = require("gulp-terser");
+const browsersync = require("browser-sync").create();
 
 // Sass Task
 function scssTask() {
-  return src('sass/main.scss', { sourcemaps: true })
+  return src("sass/main.scss", { sourcemaps: true })
     .pipe(sass())
     .pipe(postcss([cssnano()]))
-    .pipe(dest('public/assets/styles', { sourcemaps: '.' }));
+    .pipe(dest("public/assets/styles", { sourcemaps: "." }));
 }
 
 // Browsersync Tasks
 function browsersyncServe(cb) {
   browsersync.init({
     server: {
-      baseDir: './public',
+      baseDir: "./public",
     },
   });
   cb();
@@ -30,8 +30,8 @@ function browsersyncReload(cb) {
 
 // Watch Task
 function watchTask() {
-  watch('*.html', browsersyncReload);
-  watch(['sass/**/*.scss'], series(scssTask, browsersyncReload));
+  watch("*.html", browsersyncReload);
+  watch(["sass/**/*.scss"], series(scssTask, browsersyncReload));
 }
 
 // Default Gulp task
